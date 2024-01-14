6dbf85c615f615e4712991a20451c35d82b45215
laodemuhff <ldmuhfarhanfy@gmail.com>
2024-01-14 13:38:20 +0700

    changes name

--- app/controllers/v1/admin/auth.v1.admin.controller.js
+++ app/controllers/v1/admin/auth.v1.admin.controller.js
@@ -1,7 +1,7 @@
 exports.getProfile = (req, res) => {
     try {
         const data = {
-            name: 'La Ode Muhammad Farhan Fauzan. S.Kom, M.Kom',
+            name: 'La Ode Muhammad Farhan Fauzan',
             position: 'Middle Software Engineer'
         }
         return sendSuccess(res, 200, data);
